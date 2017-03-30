e8da872787f75bc1b02a583e482d6c62e4e13beb
Praveen Pendyala <npkpendyala@gmail.com>
2017-03-29 22:31:48 -0700

    Correcting the author name

--- Contacts/AppDelegate.m
+++ Contacts/AppDelegate.m
@@ -2,7 +2,7 @@
 //  AppDelegate.m
 //  Contacts
 //
-//  Created by Tech Team on 3/29/17.
+//  Created by Praveen Pendyala on 3/29/17.
 //  Copyright © 2017 Praveen Pendyala. All rights reserved.
 //
 
--- Contacts/ViewController.m
+++ Contacts/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  Contacts
 //
-//  Created by Tech Team on 3/29/17.
+//  Created by Praveen Pendyala on 3/29/17.
 //  Copyright © 2017 Praveen Pendyala. All rights reserved.
 //
 
